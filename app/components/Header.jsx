6dcee7a35197bd4e45e590645e0f37bbf0a60063
parzavel-7 <surajganesh404@gmail.com>
2025-08-31 08:05:42 +0545

    new resume added

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -72,7 +72,7 @@ const Header = ({ isDarkMode, setIsDarkMode }) => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 0.84 }}
-          href="/Suraj_Ganesh_Resume.pdf"
+          href="/my_resume.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-white dark:bg-[#1c1c1c]"
         >
